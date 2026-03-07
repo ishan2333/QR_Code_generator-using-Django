a7107a07be212123d181b488f438268f601c4785
ishan2333 <ishankhedkar2333@gmail.com>
2026-03-08 00:22:43 +0530

    added qrcode library

--- QR_GENERATOR/app/views.py
+++ QR_GENERATOR/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+import qrcode
 # Create your views here.
 def home(req):
+    data = 
+    img = qrcode.make(data)
     li = {"name" : "Ishan"}
     return render(req , "index.html" ,li)
