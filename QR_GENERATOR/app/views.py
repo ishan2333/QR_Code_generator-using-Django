1c58d575a948208ca2dad69b42a68048e318abb8
ishan2333 <ishankhedkar2333@gmail.com>
2026-03-08 21:01:43 +0530

    some changes to display qr in html

--- QR_GENERATOR/app/views.py
+++ QR_GENERATOR/app/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render ,HttpResponse
 import qrcode
 # Create your views here.
-def home(req):
-    data = 
-    img = qrcode.make(data)
-    li = {"name" : "Ishan"}
-    return render(req , "index.html" ,li)
+def process_form_view(request):
+    if request.method == 'POST':
+        user_input_data = request.POST.get('user_input')
+        print(user_input_data)
+        qrimg = qrcode.make(user_input_data)
+        
+    return render(request , "index.html")
