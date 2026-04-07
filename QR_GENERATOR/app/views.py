0a0ad3c4531d10b4b02df17b73a0678bc51c39c7
ishan2333 <ishankhedkar2333@gmail.com>
2026-04-08 00:09:48 +0530

    Backend generating qr

--- QR_GENERATOR/app/views.py
+++ QR_GENERATOR/app/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render ,HttpResponse
 import qrcode
+from io import BytesIO
+import base64
+
 # Create your views here.
 def process_form_view(request):
+    qr_code = None
     if request.method == 'POST':
-        user_input_data = request.POST.get('user_input')
-        print(user_input_data)
-        qrimg = qrcode.make(user_input_data)
-        
-    return render(request , "index.html")
+        data = request.POST.get('user_input')
+        print(data)
+        qr = qrcode.make(data)
+        buffer = BytesIO()
+        qr.save(buffer, format="PNG")
+        qr_code = base64.b64encode(buffer.getvalue()).decode()
+
+    return render(request, "index.html",{"qr_code" : qr_code})
